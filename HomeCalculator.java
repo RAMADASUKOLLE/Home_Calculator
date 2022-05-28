239861e431d5542e7edd76dd022ba2317844826d
unknown <RAMADASU.neo@gmail.com>
2022-05-28 23:36:19 +0530

    added division operation

--- HomeCalculator.java
+++ HomeCalculator.java
@@ -5,5 +5,7 @@ public static void main(String args[])
 	System.out.println("Addition is "+(10+2));
 	
 	System.out.println("Multiplication is "+(10*2));
+	
+	System.out.println("Division is "+(10/2));
 }
 }
